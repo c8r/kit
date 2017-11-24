22f563d287eb1cc4709d039193b47de03ce5fce4
John Otander <johnotander@gmail.com>
2017-11-24 13:00:09 -0700

    Add ability to specify all components shown in render and code

--- src/ComponentEditor.js
+++ src/ComponentEditor.js
@@ -36,7 +36,9 @@ const ComponentEditor = ({
     theme
   })
 
-  const code = editor.examples[editor.currentExample]
+  const code = editor.currentExample === 'all'
+    ? editor.examples.join("\n\n")
+    : editor.examples[editor.currentExample]
 
   return (
     <Div my={4}>
@@ -54,19 +56,34 @@ const ComponentEditor = ({
       Examples:
 
       {editor.examples.length > 1 && (
-        editor.examples.map((ex, i) =>
+        <Div>
           <a
             href='#!'
-            children={i}
+            children='All'
             onClick={() => update(state => {
               const newState = Object.assign({}, state)
 
-              newState.editors[component.name].currentExample = i
+              newState.editors[component.name].currentExample = 'all'
 
               return newState
             })}
           />
-        )
+
+          {editor.examples.map((ex, i) =>
+            <a
+              key={i}
+              href='#!'
+              children={i}
+              onClick={() => update(state => {
+                const newState = Object.assign({}, state)
+
+                newState.editors[component.name].currentExample = i
+
+                return newState
+              })}
+            />
+          )}
+        </Div>
       )}
     </Div>
   )
