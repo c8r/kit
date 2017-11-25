00cd756a38815a7069f6fe166ad242bc2635507f
John Otander <johnotander@gmail.com>
2017-11-25 10:06:16 -0700

    Add basic routing

--- src/Styleguide.js
+++ src/Styleguide.js
@@ -2,31 +2,43 @@ import React from 'react'
 import connect from 'refunk'
 
 import {
-  H1,
-  Container
+  Route
+} from 'react-router-dom'
+
+import {
+  Div
 } from '@compositor/mono'
+
+import Router from '@compositor/x0/lib/Router'
 
 import ComponentEditor from './ComponentEditor'
 
-const Styleguide = ({
-  components,
-  library,
-  editors,
-  config,
-  theme
-}) =>
-  <Container>
-    <H1 children='Components' />
-
-    {components.map(c =>
-      <ComponentEditor
-        key={c.name}
-        theme={theme}
-        component={c}
-        library={library}
-        editor={editors[c.name]}
+const Styleguide = props =>
+  <Div>
+    <Router>
+      <Route
+        exact
+        path='/'
+        render={() => <Div>hi</Div>}
       />
-    )}
-  </Container>
+
+      <Route
+        path='/components'
+        render={() =>
+          <Div
+            children={props.components.slice(0, 1).map(c =>
+              <ComponentEditor
+                key={c.name}
+                theme={props.theme}
+                component={c}
+                library={props.library}
+                editor={props.editors[c.name]}
+              />
+            )}
+          />
+        }
+      />
+    </Router>
+  </Div>
 
 export default connect(Styleguide)
