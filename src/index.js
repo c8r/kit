5e01c49a7d89a9101010df9d02755c13453c397e
John Otander <johnotander@gmail.com>
2018-01-20 19:43:21 -0700

    Nuke logging

--- src/index.js
+++ src/index.js
@@ -19,19 +19,13 @@ import {
 import processMetadata from './process-metadata'
 
 export default async (dir, opts = {}) => {
-  console.log(dir)
   dir = 'docs/components'
-  console.log(opts)
   const scope = require(opts.library)
-  console.log(scope)
   const metadata = await processMetadata(dir, scope, opts)
-  console.log(metadata)
 
   const data = await getData(dir, opts)
-  console.log(data)
   const pages = await render(data, opts)
   const result = await writePages(pages, opts)
-  console.log('bunnnng')
-  console.log(result)
+
   return result
 }
