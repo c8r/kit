82026470b33d3774f0afbcc8f9a62a7a10fe962a
John Otander <johnotander@gmail.com>
2018-01-26 01:03:25 -0700

    Add route handling for files

--- src/Styleguide.js
+++ src/Styleguide.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
+import {
+  BrowserRouter,
+  StaticRouter,
+  Route
+} from 'react-router-dom'
+
+import { Markdown } from '@compositor/markdown'
+
 import Box from './Box'
 import Flex from './Flex'
 import Layout from './Layout'
 import LiveEditor from './LiveEditor'
 
 import getMetadata from './get-metadata'
+
+const Router = typeof document === 'undefined' ? StaticRouter : BrowserRouter
 
 const defaultScope = {
   Box,
@@ -24,9 +34,35 @@ const Styleguide = ({
 
   return (
     <Layout {...scope}>
-      <pre>
-        {JSON.stringify(props, null, 2)}
-      </pre>
+      <Router>
+        <div>
+          <Route
+            exact
+            path='/'
+            render={() => <h1>hi</h1>}
+          />
+
+          {Object.keys(props.styleguide).map(name => {
+            const component = props.styleguide[name]
+
+            return (
+              <Route
+                key={component.name}
+                path={component.route}
+                render={() =>
+                  <Markdown
+                    text={component.content}
+                  />
+                }
+              />
+            )
+          })}
+
+          <pre>
+            {JSON.stringify(props, null, 2)}
+          </pre>
+        </div>
+      </Router>
     </Layout>
   )
 }
