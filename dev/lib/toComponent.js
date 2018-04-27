22971436f54d97e95d96ffea2f94f85f3ffac6f4
Brent Jackson <jxnblk@gmail.com>
2018-04-27 15:15:50 -0700

    Support rendering element exports in dev server

---
+++ dev/lib/toComponent.js
@@ -0,0 +1,8 @@
+import { isElement } from 'react-dom/test-utils'
+
+export default (elementOrComponent) =>
+  typeof elementOrComponent === 'function'
+    ? elementOrComponent
+    : isElement(elementOrComponent)
+      ? () => elementOrComponent
+      : () => false
