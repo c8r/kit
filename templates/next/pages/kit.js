6da9fafd15a6fd055bf9e4515f5ac3df5e7fe6e3
John Otander <johnotander@gmail.com>
2018-05-18 10:12:47 -0600

    Add app component and kit example

--- templates/next/pages/kit.js
+++ templates/next/pages/kit.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react'
-import { Container } from 'rebass'
+import React from 'react'
+import { Button } from 'standard-components'
+import { Library, Example, Cartesian } from '@compositor/kit'
 
-import { withLayout } from '../components'
-
-export default withLayout(() =>
-  <Container>
-  </Container>
-)
+export default () =>
+  <Library>
+    <Example name="Button">
+      <Button>Hello, world!</Button>
+    </Example>
+  </Library>
