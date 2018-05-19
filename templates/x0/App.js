6b0ddffd3a09df8b96fe88674782af43b9f7a9dd
John Otander <johnotander@gmail.com>
2018-05-19 09:44:36 -0600

    Begin implementing an improved x0 template

--- templates/x0/App.js
+++ templates/x0/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { Library, Example } from '@compositor/kit'
+import { Button } from 'standard-components'
+import { ThemeProvider } from 'styled-components'
 
+import theme from './theme'
 import Docs from './readme.md'
 
-export default () => <Docs />
+export default () =>
+  <ThemeProvider theme={theme}
--- templates/x0/readme.md
+++ templates/x0/readme.md
@@ -1,1 +1,19 @@
 # Kit + x0
+
+[Kit][kit] and [x0][x0] template.
+
+## Installation
+
+```
+npm install --global @compositor/kit
+kit init -t x0 -d my-x0-project
+```
+
+## Usage
+
+```
+npm start
+```
+
+[kit]: https://github.com/c8r/kit
+[x0]: https://github.com/c8r/x0
