3d1b200369b05ef7e5f9d43156d8b0ae21ef7874
Brent Jackson <jxnblk@gmail.com>
2018-05-20 11:33:20 -0400

    Add boilerplate for material ui

---
+++ templates/material-ui/examples/Typography.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Typography } from '../src'
+
+export default () => (
+  <Typography variant='title'>
+    Typography
+  </Typography>
+)
---
+++ templates/material-ui/src/ThemeProvider.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import theme from './theme'
+
+export default props => (
+  <MuiThemeProvider theme={theme}>
+    {props.children}
+  </MuiThemeProvider>
+)
