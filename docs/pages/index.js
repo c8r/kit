2c8427a22a8ce8cfebf8038a15824ceba75993c7
Brent Jackson <jxnblk@gmail.com>
2018-05-28 16:47:08 -0400

    Adjust site layout

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,44 +1,67 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Logo, Kit } from '@compositor/logo'
 import {
   Provider,
   Box,
   Flex,
+  Container,
   Heading,
   Caps,
   Text,
   Button,
-  BlockLink
+  BlockLink,
+  Image,
 } from 'rebass'
+import {
+  NavBar,
+  NavLink
+} from '@compositor/mono'
+
+const Video = styled.video([], {
+  display: 'block',
+  maxWidth: '100%',
+  height: 'auto',
+  borderRadius: '16px',
+})
 
 export default props => (
   <Provider>
-    <BlockLink mt={3} mb={3} href="https://compositor.io">
-      <Flex px={[3, 4, 5]} alignItems="center">
-        <Logo size={32} />
-        <Heading>
-          <Caps fontSize={10}>Compositor</Caps>
-        </Heading>
-      </Flex>
-    </BlockLink>
-    <Box color="white" bg="black" px={[3, 4, 5]} py={[4, 5]}>
-      <Kit size={256} />
-      <Heading fontSize={[5, 6, 7, 8]}>Kit</Heading>
-      <Text fontSize={[3, 4]} fontWeight="bold" mb={4}>
-        Tools for developing, documenting, and testing React component
-        libraries
-      </Text>
-      <Button
-        is="a"
-        href="https://github.com/c8r/kit"
-        fontSize={2}
-        px={4}
-        py={3}
-        color="black"
-        bg="white"
-      >
-        GitHub
-      </Button>
+    <NavBar>
+      <NavLink href='/'>Kit</NavLink>
+      <NavLink href='https://github.com/c8r/kit'>GitHub</NavLink>
+    </NavBar>
+    <Box color="black" bg="white" px={[3, 4 ]} py={4}>
+      <Container maxWidth={1024}>
+        <Video
+          autoPlay
+          loop
+          muted
+          playsInline
+          poster='demo.gif'
+          src='demo-small.mp4'
+        />
+        <Heading mt={3} fontSize={[5, 6, 7, 8]}>Kit</Heading>
+        <Text fontSize={[3, 4]} fontWeight="bold" mb={4}>
+          Tools for developing, documenting, and testing React component
+          libraries
+        </Text>
+        <Button
+          is="a"
+          href="https://github.com/c8r/kit"
+          fontSize={2}
+          px={4}
+          py={3}
+          bg="black"
+        >
+          GitHub
+        </Button>
+        <Flex py={4}>
+          <Box mx='auto' />
+          <Kit size={128} />
+          <Box mx='auto' />
+        </Flex>
+      </Container>
     </Box>
   </Provider>
 )
