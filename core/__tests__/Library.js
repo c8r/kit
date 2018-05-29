c160ceaf58d4378b20d715ae230dd9caeef9eea6
John Otander <johnotander@gmail.com>
2018-05-29 11:37:59 -0600

    Modify State component tests

--- core/__tests__/Library.js
+++ core/__tests__/Library.js
@@ -3,12 +3,13 @@ import { render } from 'react-testing-library'
 import { StaticRouter } from 'react-router-dom'
 import { Library, Example, Detail } from '../src'
 
-test('Library renders', t => {
-  const json = renderJSON(<Library />)
-  t.snapshot(json)
+test('Library renders', () => {
+  const { container } = render(<Library />)
+
+  expect(container).toMatchSnapshot()
 })
 
-test('Example renders', t => {
+test('Example renders', () => {
   const { container } = render(
     <Example name="Hello">
       <h1>Hello</h1>
@@ -18,7 +19,7 @@ test('Example renders', t => {
   expect(container).toMatchSnapshot()
 })
 
-test('Detail renders', t => {
+test('Detail renders', () => {
   const { container } = render(
     <StaticRouter context={{}}>
       <Detail />
