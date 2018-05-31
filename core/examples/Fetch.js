514a1984f1732fe749e3f750ef4e9daf7fb21732
John Otander <johnotander@gmail.com>
2018-05-31 09:15:28 -0600

    Add docs for Fetch, improve example

--- core/examples/Fetch.js
+++ core/examples/Fetch.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import Editor from '@compositor/react-editor'
+
 import { Fetch, Debug } from '../src'
 
+const Error = () => <h1>Error</h1>
+const Loading = () => <h1>Loading</h1>
+const Empty = () => <h1>No team members</h1>
+
 export default props => (
-  <Fetch url="https://reqres.in/api/users?page=2">
+  <Fetch url="https://compositor.fyi">
     {
       ({
-        fetchState,
         loading,
         error,
         data
-      }) => (
-        <React.Fragment>
-          <h1>{fetchState}</h1>
-          <hr />
-          <h1>Loading</h1>
-          <Debug>{loading}</Debug>
-          <hr />
-          <h1>Error</h1>
-          <Debug>{error}</Debug>
-          <hr />
-          <h1>Data</h1>
-          {data && data.page}
-          <Debug log>{data}</Debug>
-        </React.Fragment>
-      )
+      }) => {
+        if (error) { return <Error /> }
+        if (loading) { return <Loading /> }
+        if (data.team.length === 0) { return <Empty /> }
+
+        return (
+          <React.Fragment>
+            <h1>Team</h1>
+            <ul>
+              {data.team.map(({ name }) =>
+                <li key={name}>{name}</li>
+              )}
+            </ul>
+          </React.Fragment>
+        )
+      }
     }
   </Fetch>
 )
