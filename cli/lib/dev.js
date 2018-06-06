fb6d3400aa67f65b32480899a86fced48190d8b2
Brent Jackson <jxnblk@gmail.com>
2018-06-06 17:26:07 -0400

    cli: remove babel build step

--- cli/lib/dev.js
+++ cli/lib/dev.js
@@ -2,7 +2,7 @@ const path = require('path')
 const { dev } = require('@compositor/x0')
 const webpack = require('webpack')
 
-const dirname = path.join(__dirname, '../dist')
+const dirname = path.join(__dirname, '../src')
 const modes = {
   library: path.join(dirname, 'Library'),
   sandbox: path.join(dirname, 'Sandbox'),
@@ -12,6 +12,27 @@ const modes = {
 
 module.exports = opts => {
   opts.webpack = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: path.resolve(__dirname, '../node_modules'),
+          include: path.resolve(__dirname),
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              'babel-preset-env',
+              'babel-preset-stage-0',
+              'babel-preset-react',
+            ].map(require.resolve),
+            plugins: [
+              'babel-plugin-macros',
+              'babel-plugin-transform-runtime'
+            ].map(require.resolve)
+          }
+        }
+      ],
+    },
     resolve: {
       alias: {
         'webpack-hot-client/client': path.join(__dirname, '../node_modules/webpack-hot-client/client')
