4206bf164fbeb4a66b691befe089fec2ea24c1f1
Vince Speelman <v@vinspee.me>
2018-06-22 23:14:55 -0400

    add "container" prop to Cartesian
    Allow the consumer to provide an optional `container` prop. This prop  is the same type as the `component` prop and behaves the same way. The `container` prop is a way for consumers to control the way each child of the Cartesian component may look. I anticipate this being used primarily for spacing and layout, when the product of the cartesian component does not have any opinions about its container.

--- core/src/Cartesian.js
+++ core/src/Cartesian.js
@@ -1,10 +1,24 @@
 import React, { Fragment } from 'react'
 import { cartesianProduct } from './util'
 
-export default ({ component, ...props }) => {
+export default ({ component, container, ...props }) => {
   const combinations = cartesianProduct(props)
   const Component = component
+  
+  if (container) {
+    const Container = container
 
+    return (
+      <Fragment>
+        {combinations.map((props, i) => (
+          <Container>
+            <Component key={i} {...props} />
+          </Container>
+        ))}
+      </Fragment>
+    )
+  }
+  
   return (
     <Fragment>
       {combinations.map((props, i) => <Component key={i} {...props} />)}
