618339bb4af2c4d09f935eaef2a183c2ffc10f2c
John Otander <johnotander@gmail.com>
2018-07-06 14:39:10 -0700

    Nuke site dir

--- site/examples/Button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Button } from 'rebass'
-
-export default () => (
-  <Button>Button</Button>
-)
--- site/examples/Text.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Text } from 'rebass'
-
-export default () => (
-  <Text>Text</Text>
-)
